9bf4da40d4927dd1ebf7a07f078b371a09100c80
linaA <hellolinita@gmail.com>
2025-03-06 10:46:57 -0500

    cors

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 import express from "express"
 import { connectDB } from "./config/db.js"
 import routes from "./routes/routes.js";
+import cors from "cors"
+
+
+
+
 
 
 const app = express();
+app.use(cors());
 connectDB()
+
+app.get("/", (req, res) => {
+    res.send("CORS configurado correctamente");
+});
 
 app.use(express.json());
 app.use('/api', routes);
